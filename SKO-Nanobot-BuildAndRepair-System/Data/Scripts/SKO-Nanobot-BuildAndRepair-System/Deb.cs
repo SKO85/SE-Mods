6dfd42bb79b221ae43d319182ad61664c4446ede
SKO85 <23386482+SKO85@users.noreply.github.com>
2024-01-22 13:38:26 +0100

    Initial commit of the mods. (#3)

---
+++ SKO-Nanobot-BuildAndRepair-System/Data/Scripts/SKO-Nanobot-BuildAndRepair-System/Deb.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox.ModAPI;
+using VRage.Utils;
+
+namespace SKONanobotBuildAndRepairSystem
+{
+    internal class Deb
+    {
+        private static readonly bool EnableDebug = false;
+
+        public static void Write(string msg)
+        {
+            if (EnableDebug)
+            {
+                MyAPIGateway.Utilities.ShowMessage("Nanobot Debug", msg);
+                MyLog.Default.WriteLineAndConsole($"Nanobot Debug: {msg}");
+            }
+        }
+    }
+}
---
+++ _Scripts/build_post.cmd
@@ -0,0 +1,20 @@
+ECHO "POST SCRIPT STARTED"
+set ProjectName=%1
+set ProjectDir=%2
+set ModFolder=%appdata%\SpaceEngineers\Mods\%ProjectName%
+set ScriptFolder=%ModFolder%\Data\Scripts\%ProjectName%
+
+rmdir "%ModFolder%" /s /q
+mkdir "%ModFolder%"
+xcopy "%ProjectDir%\..\..\..\" "%ModFolder%" /S /Y
+
+rmdir "%ScriptFolder%\bin" /s /q
+rmdir "%ScriptFolder%\obj" /s /q
+rmdir "%ScriptFolder%\Properties" /s /q
+rmdir "%ScriptFolder%\packages" /s /q
+
+del /s /q "%ScriptFolder%\*.config"
+del /s /q "%ScriptFolder%\*.svn"
+del /s /q "%ScriptFolder%\*.sln"
+del /s /q "%ScriptFolder%\*.csproj"
+del /s /q "%ScriptFolder%\*.user"
