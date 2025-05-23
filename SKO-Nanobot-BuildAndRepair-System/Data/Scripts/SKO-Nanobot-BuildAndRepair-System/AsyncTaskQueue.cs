6c8b1a3103a7a3207ac13f92ef0a0d27c6e64d59
SKO85 <23386482+SKO85@users.noreply.github.com>
2025-05-24 00:50:44 +0200

    Use older async-task-queue.

--- SKO-Nanobot-BuildAndRepair-System/Data/Scripts/SKO-Nanobot-BuildAndRepair-System/AsyncTaskQueue.cs
+++ SKO-Nanobot-BuildAndRepair-System/Data/Scripts/SKO-Nanobot-BuildAndRepair-System/AsyncTaskQueue.cs
@@ -7,17 +7,18 @@ namespace SKONanobotBuildAndRepairSystem
     public static class AsyncTaskQueue
     {
         private static readonly Queue<Action> _taskQueue = new Queue<Action>();
+        private static readonly object _lock = new object();
         private static int _runningTasks = 0;
 
         public static void Enqueue(Action action)
         {
             if (action == null) return;
 
-            lock(_taskQueue)
+            lock (_lock)
             {
                 _taskQueue.Enqueue(action);
-            }            
-            TryRunNext();            
+                TryRunNext();
+            }
         }
 
         private static void TryRunNext()
@@ -25,21 +26,14 @@ namespace SKONanobotBuildAndRepairSystem
             if (_runningTasks >= NanobotBuildAndRepairSystemMod.Settings.MaxBackgroundTasks || _taskQueue.Count == 0)
                 return;
 
-            Action task = null;
-            lock (_taskQueue)
-            {
-                task = _taskQueue.Dequeue();
-            }
+            var task = _taskQueue.Dequeue();
             _runningTasks++;
 
             MyAPIGateway.Parallel.StartBackground(() =>
             {
                 try
                 {
-                    if (task != null)
-                    {
-                        task();
-                    }
+                    task();
                 }
                 catch (Exception ex)
                 {
@@ -47,18 +41,18 @@ namespace SKONanobotBuildAndRepairSystem
                 }
                 finally
                 {
-                    if (task != null)
+                    lock (_lock)
                     {
                         _runningTasks--;
+                        TryRunNext();
                     }
-                    TryRunNext();
                 }
             });
         }
 
         public static void Clear()
         {
-            lock (_taskQueue)
+            lock (_lock)
             {
                 _taskQueue.Clear();
             }
